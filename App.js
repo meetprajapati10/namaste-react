7d005890fd1cf9ca3835116de8cb57766c18d223
Meet Prajapati <meetprajapati5243@gmail.com>
2023-08-27 17:47:36 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,22 +1,9 @@
-/* 
-    <div id='parent'>
-        <div id='child'>
-            <h1>I'm a h1 tag.</h1>
-            <h2>I'm a h2 tag</h2>
-        </div> 
-        <div id='child2'>
-            <h1>I'm a h1 tag.</h1>
-            <h2>I'm a h2 tag</h2>
-        </div>   
-    </div>
-
-
- ***********************  ReactElement(Object) => HTML(Browser Understands) ***************************  
-*/
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child" }, [
-        React.createElement("h1", {}, "I'm a h1 tag"),
+        React.createElement("h1", {}, "Namste ReactJs."),
         React.createElement("h2", {}, "I'm a h2 tag"),
     ]),
     React.createElement("div", { id: "child2" }, [
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namaste ReactJs ✌️🚀
+
+# Parcel
+
+- Dev Build.
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - Written in C++
+- Caching - Faster Builds.
+- Image optimization
+- Minification
+- Tree shaking - Remove unused code
+- Compression
+- Code splitting
+- Content hashing
+- Bundling
+- Differential bundling - support older browser
+- Diagnostics
+- Error Handling
+- HTTPs
+- Different dev and prod bundles
